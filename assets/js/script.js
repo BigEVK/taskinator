ca370451a18a6f436c9b0db8dbf28a20f66c38f3
EVK <eric@ericvankirk.com>
2022-06-15 13:05:54 -0500

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     
     event.preventDefault();
     
@@ -12,6 +12,16 @@ var createTaskHandler = function(event) {
     
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
+    var taskDataObj = {
+        name : taskNameInput,
+        type : taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
+    
+  
+};
+var createTaskEl = function(taskDataObj) {
     var listItemEl = document.createElement("li");
     
     listItemEl.className = "task-item";
@@ -20,14 +30,12 @@ var createTaskHandler = function(event) {
     
     taskInfoEl.className = "task-info";
     
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     
     listItemEl.appendChild(taskInfoEl);
     
     
     tasksToDoEl.appendChild(listItemEl);
-    
-  
-};
+}
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
